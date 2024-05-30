8774b29ae6aa736da895d030f3d4ddeb9ebe29a6
JadeCheah <tszskygypei2522@gmail.com>
2024-05-30 21:30:12 +0800

    added OnboardingScreen.js

---
+++ Screen/OnboardingScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <Onboarding
+        pages={[
+            {
+                backgroundColor: '#fff',
+                image: <Image source={require('./assets/icon.png')} />,
+                title: 'Onboarding 1',
+                subtitle: 'Done with React Native Onboarding Swiper',
+            },
+            {
+                backgroundColor: '#fff',
+                image: <Image source={require('./assets/icon.png')} />,
+                title: 'Onboarding 2',
+                subtitle: 'Done with React Native Onboarding Swiper',
+            },
+        ]}
+    />
+    )
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+});
