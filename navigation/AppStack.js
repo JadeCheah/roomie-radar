b496c155eb19db0237ea91aed007e64a3123a8cb
mariia <tomashapopova@gmail.com>
2024-06-12 08:02:59 +0800

    added likes comments and divider

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createStackNavigator();
+//something missing
 
 const FeedStack = ({ navigation }) => (
   <Stack.Navigator>
