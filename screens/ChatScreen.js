d531337d09c055da3a2d9bc85947bd11c16967e2
mariia <tomashapopova@gmail.com>
2024-06-30 20:15:44 +0800

    test

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -2,68 +2,174 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp, query, orderBy, onSnapshot, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { useAuth } from '../navigation/AuthProvider';
-import { firestore } from '../firebaseConfig'; 
+import { firestore } from '../firebaseConfig';
+
 
 const ChatScreen = ({ route }) => {
-  const [messages, setMessages] = useState([]);
-  const { user } = useAuth();
-  const { recipientId, recipientName } = route.params;
+ const [messages, setMessages] = useState([]);
+ const { user } = useAuth();
+ const { conversationId, recipientId, recipientName } = route.params;
 
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-    messages.forEach(async message => {
-      try {
-        console.log(user);  // See what properties are available
-        await addDoc(collection(firestore, 'messages'), {
-          text: message.text,
-          createdAt: serverTimestamp(),
-          senderId: user.uid,  
-          recipientId: recipientId,
-          recipientName: recipientName,
-           senderName: user.displayName,  
-          senderProfilePhoto: user.photoURL,
-        });
-      } catch (error) {
-        console.error("Error adding message to Firestore:", error);
-      }
-    });
-  }, [recipientId, user]);
 
-  const renderSend = (props) => (
-    <Send {...props}>
-      <View style={{ marginBottom: 5, marginRight: 5 }}>
-        <MaterialCommunityIcons name="send-circle" size={32} color="#2e64e5" />
-      </View>
-    </Send>
-  );
+ useEffect(() => {
+   const messagesRef = collection(firestore, `conversations/${conversationId}/messages`);
+   const q = query(messagesRef, orderBy('createdAt', 'asc'));
 
-  const renderBubble = (props) => (
-    <Bubble {...props} wrapperStyle={{ right: { backgroundColor: '#2e64e5' } }} textStyle={{ right: { color: '#fff' } }} />
-  );
 
-  const scrollToBottomComponent = () => (
-    <MaterialCommunityIcons name="chevron-double-down" size={22} color="#333" />
-  );
+   const unsubscribe = onSnapshot(q, (snapshot) => {
+     const incomingMessages = snapshot.docs.map(doc => ({
+       _id: doc.id,
+       text: doc.data().text,
+       createdAt: doc.data().createdAt ? doc.data().createdAt.toDate() : new Date(),
+       user: {
+         _id: doc.data().senderId,
+         name: doc.data().senderName,
+         avatar: doc.data().senderProfilePhoto,
+       },
+     }));
+     setMessages(GiftedChat.append(messages, incomingMessages));
+   });
 
-  return (
-    <GiftedChat
-      messages={messages}
-      onSend={onSend}
-      user={{ _id: user.uid, name: user.displayName, avatar: user.photoURL }}
-      renderBubble={renderBubble}
-      alwaysShowSend
-      renderSend={renderSend}
-      scrollToBottom
-      scrollToBottomComponent={scrollToBottomComponent}
-    />
-  );
+
+   return () => unsubscribe();
+ }, [conversationId]);
+
+
+ function useMessages(conversationId) {
+   const [messages, setMessages] = useState([]);
+    useEffect(() => {
+     const messagesRef = collection(firestore, `conversations/${conversationId}/messages`);
+     const q = query(messagesRef, orderBy('createdAt', 'asc'));
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+       const fetchedMessages = snapshot.docs.map(doc => ({
+         id: doc.id,
+         ...doc.data()
+       }));
+       setMessages(fetchedMessages);
+     });
+      return () => unsubscribe();  // Clean up the listener when the component unmounts
+   }, [conversationId]);
+    return messages;
+ }
+
+
+ // const onSend = useCallback(async (messages = []) => {
+ //   messages.forEach(async message => {
+ //     const messageData = {
+ //       text: message.text,
+ //       createdAt: serverTimestamp(),
+ //       senderId: user.uid,
+ //       senderName: user.displayName,
+ //       senderProfilePhoto: user.photoURL,
+ //     };
+ //     // Add message to the messages sub-collection
+ //     const newMessageRef = await addDoc(collection(firestore, `conversations/${conversationId}/messages`), messageData);
+
+
+ //     // Check if conversation exists and update or create it
+ //     const conversationRef = doc(firestore, `conversations/${conversationId}`);
+ //     const conversationSnap = await getDoc(conversationRef);
+ //     if (conversationSnap.exists()) {
+ //       // Update the existing conversation
+ //       await updateDoc(conversationRef, {
+ //         lastMessage: message.text,
+ //         lastMessageTime: serverTimestamp()
+ //       });
+ //     } else {
+ //       // Create a new conversation if not exists (adjust fields as needed)
+ //       await setDoc(conversationRef, {
+ //         participantIds: [user.uid, recipientId], // Example: Adjust based on your app logic
+ //         participantNames: [user.displayName, recipientName], // Example: Adjust as needed
+ //         lastMessage: message.text,
+ //         lastMessageTime: serverTimestamp()
+ //       });
+ //     }
+ //   });
+ // }, [conversationId, user, recipientId, recipientName]);
+  const onSend = useCallback(async (messages = []) => {
+   messages.forEach(async message => {
+     const messageData = {
+       text: message.text,
+       createdAt: serverTimestamp(),
+       senderId: user.uid,
+       senderName: user.displayName,
+       senderProfilePhoto: user.photoURL,
+     };
+      // Fetch all conversations where the current user is a participant
+     const conversationsRef = collection(firestore, 'conversations');
+     const q = query(conversationsRef, where("participantIds", "array-contains", user.uid));
+     const querySnapshot = await getDocs(q);
+     let conversationFound = false;
+     let existingConversationId = null;
+      // Manually check each conversation to find if the recipient is also a participant
+     querySnapshot.forEach(doc => {
+       const participants = doc.data().participantIds;
+       if (participants.includes(recipientId) && participants.length === 2) {
+         conversationFound = true;
+         existingConversationId = doc.id;
+       }
+     });
+      if (conversationFound && existingConversationId) {
+       // Add message to the existing conversation
+       await addDoc(collection(firestore, `conversations/${existingConversationId}/messages`), messageData);
+       // Update the existing conversation
+       await updateDoc(doc(firestore, `conversations/${existingConversationId}`), {
+         lastMessage: message.text,
+         lastMessageTime: serverTimestamp()
+       });
+     } else {
+       // Create a new conversation if it does not exist
+       const newConversationRef = await addDoc(conversationsRef, {
+         participantIds: [user.uid, recipientId],
+         participantNames: [user.displayName, recipientName],
+         lastMessage: message.text,
+         lastMessageTime: serverTimestamp()
+       });
+       // Add the first message to the new conversation
+       await addDoc(collection(firestore, `conversations/${newConversationRef.id}/messages`), messageData);
+     }
+   });
+ }, [user, recipientId, recipientName]);
+
+
+ 
+ const renderSend = (props) => (
+   <Send {...props}>
+     <View style={{ marginBottom: 5, marginRight: 5 }}>
+       <MaterialCommunityIcons name="send-circle" size={32} color="#2e64e5" />
+     </View>
+   </Send>
+ );
+
+
+ const renderBubble = (props) => (
+   <Bubble {...props} wrapperStyle={{ right: { backgroundColor: '#2e64e5' } }} textStyle={{ right: { color: '#fff' } }} />
+ );
+
+
+ const scrollToBottomComponent = () => (
+   <MaterialCommunityIcons name="chevron-double-down" size={22} color="#333" />
+ );
+
+
+ return (
+   <GiftedChat
+     messages={messages}
+     onSend={onSend}
+     user={{ _id: user.uid, name: user.displayName, avatar: user.photoURL }}
+     renderBubble={renderBubble}
+     alwaysShowSend
+     renderSend={renderSend}
+     scrollToBottom
+     scrollToBottomComponent={scrollToBottomComponent}
+   />
+ );
 };
+
 
 export default ChatScreen;
 
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-});
+
 
--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,135 +1,125 @@
 
+
 import React, { useEffect, useState } from 'react';
 import { FlatList, Text, TouchableOpacity, View, StyleSheet, Alert } from 'react-native';
-import { firestore } from '../firebaseConfig'; 
+import { firestore } from '../firebaseConfig';
 import { query, collection, where, onSnapshot, orderBy, getDocs } from 'firebase/firestore';
 import { useAuth } from '../navigation/AuthProvider';
 
+
 const MessageScreen = ({ navigation }) => {
-    const [chats, setChats] = useState([]);
-    const { user } = useAuth();  // Fetching the current authenticated user
-
-    const initiateConversation = async (email) => {
-        const usersRef = collection(firestore, 'users');
-        const q = query(usersRef, where('email', '==', email));
-        
-        const querySnapshot = await getDocs(q);
-        if (!querySnapshot.empty) {
-            const userData = querySnapshot.docs[0].data();
-            const userId = querySnapshot.docs[0].id;
-            
-            // Optionally check for an existing conversation or create a new one
-            navigation.navigate('ChatScreen', {
-                recipientId: userId,
-                recipientName: userData.displayName || "Unknown",
-            });
-        } else {
-            Alert.alert("Invalid email", "No user found with this email address.");
-        }
-    };
-
-    const promptForEmail = () => {
-        Alert.prompt(
-            'Start New Conversation',
-            'Enter the email address of the user:',
-            [
-                {
-                    text: 'Cancel',
-                    style: 'cancel',
-                },
-                {
-                    text: 'OK',
-                    onPress: email => initiateConversation(email),
-                },
-            ],
-            'plain-text'
-        );
-    };
+   const [chats, setChats] = useState([]);
+   const { user } = useAuth();
 
 
-    useEffect(() => {
-        if (!user) return;
-    
-        const conversationsRef = collection(firestore, 'conversations');
+   const initiateConversation = async (email) => {
+       const usersRef = collection(firestore, 'users');
+       const q = query(usersRef, where('email', '==', email));
+       const querySnapshot = await getDocs(q);
+      
+       if (!querySnapshot.empty) {
+           const userData = querySnapshot.docs[0].data();
+           const userId = querySnapshot.docs[0].id;
+           navigation.navigate('ChatScreen', {
+               recipientId: userId,
+               recipientName: userData.displayName || "Unknown",
+           });
+       } else {
+           Alert.alert("Invalid email", "No user found with this email address.");
+       }
+   };
 
-        const q = query(collection(firestore, 'users'), orderBy('userName', 'desc'));
+
+   const promptForEmail = () => {
+       Alert.prompt(
+           'Start New Conversation',
+           'Enter the email address of the user:',
+           [
+               {
+                   text: 'Cancel',
+                   style: 'cancel',
+               },
+               {
+                   text: 'OK',
+                   onPress: email => initiateConversation(email),
+               },
+           ],
+           'plain-text'
+       );
+   };
 
 
-        // const q = query(
-        //     conversationsRef,
-        //     where("participantIds", "array-contains", user.uid),
-        //     orderBy("lastMessageTime", "desc")
-        // );
-    
-        const unsubscribe = onSnapshot(q, snapshot => {
-            const fetchedChats = snapshot.docs.map(doc => ({
-                id: doc.id,
-                userName: "Conversation", // You need another query to fetch user details unless stored in each conversation
-                lastMessage: doc.data().lastMessage,
-                lastMessageTime: doc.data().lastMessageTime
-            }));
-            setChats(fetchedChats);
-        });
-    
-        return () => unsubscribe();
-    }, [user]);
+   useEffect(() => {
+       if (!user) return;
+       const conversationsRef = collection(firestore, 'conversations');
+       const q = query(conversationsRef, where("participantIds", "array-contains", user.uid), orderBy("lastMessageTime", "desc"));
 
-    const goToChat = (userId, userName) => {
-        navigation.navigate('ChatScreen', {
-            recipientId: userId,
-            recipientName: userName
-        });
-    };
-    
 
-    return (
-        <View style={StyleSheet.container}>
-            <TouchableOpacity onPress={promptForEmail} style={styles.addButton}>
-                <Text style={styles.addButtonText}>+</Text>
-            </TouchableOpacity>
-        <FlatList
-            data={chats}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('Chat', {
-                        recipientId: item.id,
-                        recipientName: item.userName
-                    })}
-                >
-                    <Text>{item.userName}</Text>
-                    <Text>{item.lastMessage}</Text>
-                </TouchableOpacity>
-            )}
-        />
-        </View>
-    );
+       const unsubscribe = onSnapshot(q, snapshot => {
+           const fetchedChats = snapshot.docs.map(doc => ({
+               id: doc.id,
+               userName: doc.data().participantNames.join(', '), // assuming participantNames is an array of names
+               lastMessage: doc.data().lastMessage,
+               lastMessageTime: doc.data().lastMessageTime ? doc.data().lastMessageTime.toDate() : new Date() // Fallback to current date if null
+           }));
+           setChats(fetchedChats);
+       });
+
+
+       return () => unsubscribe();
+   }, [user]);
+
+
+   return (
+       <View style={styles.container}>
+           <TouchableOpacity onPress={promptForEmail} style={styles.addButton}>
+               <Text style={styles.addButtonText}>+</Text>
+           </TouchableOpacity>
+           <FlatList
+               data={chats}
+               keyExtractor={item => item.id}
+               renderItem={({ item }) => (
+                   <TouchableOpacity
+                       onPress={() => navigation.navigate('ChatScreen', {
+                           recipientId: item.id,
+                           recipientName: item.userName
+                       })}
+                   >
+                       <Text style={styles.userName}>{item.userName}</Text>
+                       <Text style={styles.lastMessage}>{item.lastMessage}</Text>
+                   </TouchableOpacity>
+               )}
+           />
+       </View>
+   );
 };
+
 
 export default MessageScreen;
 
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10
-    },
-    addButton: {
-        padding: 10,
-        position: 'absolute',
-        right: 10,
-        top: 10,
-        zIndex: 1
-    },
-    addButtonText: {
-        fontSize: 24,
-        color: '#2e64e5'
-    },
-    userName: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    lastMessage: {
-        fontSize: 14,
-        color: '#666'
-    }
+   container: {
+       flex: 1,
+       padding: 10
+   },
+   addButton: {
+       padding: 10,
+       position: 'absolute',
+       right: 10,
+       top: 10,
+       zIndex: 1
+   },
+   addButtonText: {
+       fontSize: 24,
+       color: '#2e64e5'
+   },
+   userName: {
+       fontSize: 18,
+       fontWeight: 'bold'
+   },
+   lastMessage: {
+       fontSize: 14,
+       color: '#666'
+   }
 });
